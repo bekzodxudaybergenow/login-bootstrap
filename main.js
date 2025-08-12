aaddd8ef94714fd095783e1415ec252fe5e998dd
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2025-08-12 22:07:24 +0500

    Add javascript to the Account section

--- main.js
+++ main.js
@@ -13,3 +13,23 @@
       }, false)
     })
 })()
+
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', (event) => {
+    event.preventDefault(); // form submit bo‘lmasin
+
+    const toast = new bootstrap.Toast(toastLiveExample);
+    toast.show();
+
+    toastLiveExample.addEventListener('hidden.bs.toast', () => {
+      window.location.href = "login.html";
+    }, { once: true });
+
+    setTimeout(() => {
+      window.location.href = "login.html";
+    }, 2000);
+  });
+}
